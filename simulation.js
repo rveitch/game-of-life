88e44bdd3cb388e3c5181aae8dba668120433138
Ryan Veitch <ryan.r.veitch@gmail.com>
2022-04-28 15:12:49 -0500

    [CHORE] Adds grid validation

--- simulation.js
+++ simulation.js
@@ -1,12 +1,13 @@
 import { firstGeneration } from './lib/data/grids.js';
 import { EMPTY, NEWBORN, ADULT, SENIOR } from './lib/config/constants.js';
 import { TARGET_GENERATION, LOG_ALL_GENERATIONS } from './lib/config/config.js';
-import { printGeneration } from './lib/utils/utils.js';
+import { printGeneration, validateGrid } from './lib/utils/utils.js';
 
 /**
  * Executes the Game of Life Simulation
  */
 function runSimulation() {
+  validateGrid(firstGeneration); // Ensure we are starting with a valid grid
   let currentGeneration = 1;
   let currentGrid = firstGeneration;
 
